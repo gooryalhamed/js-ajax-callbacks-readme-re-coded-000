59454a3e40a216da645d85ee29b31672d9dfc5c0
Goory al Hamed <gooryalhamed@hotmail.com>
2017-02-20 18:59:01 +0300

    Done.

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  /*
+  $.get('sentence.html', function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    $("#sentences").html(response);
+    });
+    */
+    $.get('sentence.html', function(response){
+
+    });
+ 
+ var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
+ 
+$.get(url)
+  .done(function(data) {
+    console.log("Done");
+    console.log(data);
+  });
+});
